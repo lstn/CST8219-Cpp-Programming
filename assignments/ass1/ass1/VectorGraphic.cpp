11da8ebf85a5bf49c093cad61dcd7e5b816793bc
Lucas Estienne <lucas@estienne.sh>
2016-10-15 15:20:59 -0400

    working add

--- assignments/ass1/ass1/VectorGraphic.cpp
+++ assignments/ass1/ass1/VectorGraphic.cpp
@@ -6,86 +6,67 @@
 #include "VectorGraphic.h"
 using namespace std;
 
-VectorGraphic::VectorGraphic(){}
+VectorGraphic::VectorGraphic(){
+	this->numGraphicElements = 0;
+	this->pElements = nullptr;
+}
 
 void VectorGraphic::AddGraphicElement(){
-	GraphicElement newGraphicElement;
-	VectorGraphic vectorBuffer;
-	char inputBuffer[1024];
-	bool isValidName = false, isValidNumLines = false, isValidCoords = false;
-	int numLines, xCoord, yCoord, i;
-	unsigned int j, jj;
-
+	unsigned int i, j;
 	cout << "Adding A Graphic Element" << endl;
 
-	while (!isValidName){
-		cout << "Please enter the name of the new GraphicElement(<256 characters): ";
-		cin >> inputBuffer;
+	/* begin increasing array memory */
+	GraphicElement* newElements = new GraphicElement[this->numGraphicElements+1];
 
-		if (strlen(inputBuffer) > 255 || strlen(inputBuffer) < 1) { // Leave 1 char to append line termination '\0'.
-			cout << "Invalid name length.\n" << endl;
-		}
-		else { isValidName = true; }
+	memcpy(newElements, this->pElements, this->numGraphicElements * sizeof(GraphicElement));
+
+	for (i = 0; i < this->numGraphicElements; i++){ /* swap lines */
+		newElements[i].pLines = new Line[newElements[i].numLines];
+		for (j = 0; j < newElements[i].numLines; j++) newElements[i].pLines[j] = this->pElements[i].pLines[j];
 	}
 
-	while (!isValidNumLines){
-		cout << "How many lines are there in the new GraphicElement? ";
-		cin >> numLines; 
+	this->numGraphicElements++;
+	unsigned int elemPos = this->numGraphicElements - 1; // current element array pos
 
-		if (numLines < 1) { /* empty, -1 or input contains no integers*/
+	delete[] this->pElements;
+	this->pElements = newElements;
+	
+	for (i = 0; i < elemPos; i++){ /* swap the lines back */
+		this->pElements[i].pLines = newElements[i].pLines;
+		for (j = 0; j < this->pElements[i].numLines; j++) this->pElements[i].pLines[j] = newElements[i].pLines[j];
+	}
+	/* done increasing array memory */
+
+	cout << "Please enter the name of the new GraphicElement(<256 characters): ";
+	cin >> this->pElements[elemPos].name; // take name in
+
+	bool isValidNumLines = false;
+	while (!isValidNumLines){ // validate number of lines input
+		cout << "How many lines are there in the new GraphicElement? ";
+		cin >> this->pElements[elemPos].numLines;
+
+		if (this->pElements[elemPos].numLines < 1) {
+			/*empty, -1 or input contains no integers*/
 			cout << endl << "Invalid input, please input an integer number.";
 		}
 		else { isValidNumLines = true; }
 	}
 
-	strcpy_s(newGraphicElement.name, inputBuffer);
-	newGraphicElement.numLines = numLines;
-	newGraphicElement.pLines = new Line[newGraphicElement.numLines];
+	this->pElements[elemPos].pLines = new Line[this->pElements[elemPos].numLines]; // init pLines
 
-	for (i = 0; i < numLines; i++) {
+	for (unsigned int i = 0; i < this->pElements[elemPos].numLines; i++) {
 
 		cout << "Please enter the x coord of the start point of line index " << i << ": ";
-		cin >> newGraphicElement.pLines[i].start.x;
+		cin >> this->pElements[elemPos].pLines[i].start.x;
 
 		cout << "Please enter the y coord of the start point of line index " << i << ": ";
-		cin >> newGraphicElement.pLines[i].start.y;
+		cin >> this->pElements[elemPos].pLines[i].start.y;
 
 		cout << "Please enter the x coord of the end point of line index " << i << ": ";
-		cin >> newGraphicElement.pLines[i].end.x;
+		cin >> this->pElements[elemPos].pLines[i].end.x;
 
 		cout << "Please enter the y coord of the end point of line index " << i << ": ";
-		cin >> newGraphicElement.pLines[i].end.y;
-	}
-
-	vectorBuffer.numGraphicElements = (this->numGraphicElements > 0) ? this->numGraphicElements : 0;
-	vectorBuffer.pElements = new GraphicElement[vectorBuffer.numGraphicElements];
-
-	for (j = 0; j < vectorBuffer.numGraphicElements; j++) { /* copy pImage to buffer so we can reinit pImage */
-		vectorBuffer.pElements[j] = this->pElements[j];
-		vectorBuffer.pElements[j].pLines = new Line[this->pElements[j].numLines];
-
-		for (jj = 0; jj < this->pElements[j].numLines; jj++) { /* copy pImage to buffer so we can reinit pImage */
-			vectorBuffer.pElements[j].pLines[jj] = this->pElements[j].pLines[jj];
-		}
-	}
-	
-	this->numGraphicElements += 1; /* increase the number of graphic elements*/
-
-	this->pElements = new GraphicElement[this->numGraphicElements];
-	for (j = 0; j < vectorBuffer.numGraphicElements; j++) { /* copy pImage to buffer so we can reinit pImage */
-		this->pElements[j] = vectorBuffer.pElements[j];
-		this->pElements[j].pLines = new Line[vectorBuffer.pElements[j].numLines];
-
-		for (jj = 0; jj < this->pElements[j].numLines; jj++) { /* copy pImage to buffer so we can reinit pImage */
-			this->pElements[j].pLines[jj] = vectorBuffer.pElements[j].pLines[jj];
-		}
-	}
-
-	this->pElements[j] = newGraphicElement;
-	this->pElements[j].pLines = new Line[newGraphicElement.numLines];
-
-	for (jj = 0; jj < newGraphicElement.numLines; jj++) { /* copy pImage to buffer so we can reinit pImage */
-		this->pElements[j].pLines[jj] = newGraphicElement.pLines[jj];
+		cin >> this->pElements[elemPos].pLines[i].end.y;
 	}
 
 }
@@ -104,7 +85,7 @@ void VectorGraphic::ReportVectorGraphic(){
 	for (i = 0; i < this->numGraphicElements; i++){
 		cout << "Reporting element #" << i << endl;
 		cout << "Graphic Element name: " << (this->pElements[i]).name << endl;
-		for (j = 0; j < (this->pElements[i]).numLines; j++){
+		for (j = 0; j < this->pElements[i].numLines; j++){
 			cout << "Line #" << j << " start x: " << this->pElements[i].pLines[j].start.x << endl;
 			cout << "Line #" << j << " start y: " << this->pElements[i].pLines[j].start.y << endl;
 			cout << "Line #" << j << " end x: " << this->pElements[i].pLines[j].end.x << endl;
