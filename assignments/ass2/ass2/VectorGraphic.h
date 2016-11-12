5247475943a7a779d876b1b8cd48f70e68e7d641
Lucas Estienne <lucas@estienne.sh>
2016-11-12 09:04:25 -0500

    assign 2

---
+++ assignments/ass2/ass2/VectorGraphic.h
@@ -0,0 +1,20 @@
+#if#include <iostream>
+using namespace std;
+
+class VectorGraphic
+{
+	unsigned int numGraphicElements;
+	GraphicElement* pElements;
+public:
+	VectorGraphic() :pElements(nullptr), numGraphicElements(0){}
+	~VectorGraphic()
+	{
+		if (pElements)
+			delete[]pElements;
+	}
+	void AddGraphicElement();
+	void DeleteGraphicElement();
+	void operator+=(GraphicElement&); // add a Graphic Element at the end
+	GraphicElement& operator[](int index);
+	friend ostream& operator<<(ostream&, VectorGraphic&);
+};
